853e9e61bd14ce148c80ba3a81d959623643733f
Philip Smerud <phil@netlogicsps.com>
2026-02-17 07:37:56 -0600

    Add WeatherApi class and spec

---
+++ app/lib/weather_api.rb
@@ -0,0 +1,52 @@
+require 'net/http'
+
+class WeatherApi
+  attr_reader :address, :days, :version
+
+  API_KEY = ENV["WEATHER_API_KEY"] || ""
+  DOMAIN = "api.weatherapi.com"
+  PROTOCOL = "http"
+  DEFAULT_VERSION = "v1"
+  DEFAULT_DAYS = "1"
+
+  delegate :city, :state, :zip, to: :address
+
+  def initialize(address:, days: DEFAULT_DAYS, version: DEFAULT_VERSION)
+    @address = address
+    @days = days
+    @version = version
+  end
+
+  def lookup
+    response = Net::HTTP.get_response(request_uri)
+    JSON.parse response.body
+  end
+
+  private
+
+  def request_uri
+    URI("#{protocol}://#{domain}/#{self.version}/forecast.json?key=#{api_key}&q=#{ordinal_search_terms}&days=#{self.days}&alerts=yes")
+  end
+
+  def ordinal_search_terms
+    return "#{zip}" if zip.present?
+    return "#{city} #{state}" if city_and_state?
+    return "#{city}" if city.present?
+  end
+
+  def city_and_state?
+    city.present? && state.present?
+  end
+
+  def api_key
+    API_KEY
+  end
+
+  def domain
+    DOMAIN
+  end
+
+  def protocol
+    PROTOCOL
+  end
+end
---
+++ spec/lib/weather_api_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe WeatherApi do
+  let(:address) { build :address }
+  subject(:weather_api_klass) { WeatherApi }
+
+  describe '.new' do
+    context 'required kwargs' do
+      let(:constructor_args) {{ address: address  }}
+
+      it "requires address kwargs" do
+        expect { subject.new(**constructor_args) }.not_to raise_error
+        constructor_args.except! :address
+        expect { subject.new(**constructor_args) }.to raise_error StandardError, /missing keyword: :address/
+      end
+    end
+
+    context 'optional kwargs' do
+      it "accepts days" do
+        expect { subject.new(address: address, days: 5) }.not_to raise_error
+      end
+
+      it "accepts version" do
+        expect { subject.new(address: address, version: 2) }.not_to raise_error
+      end
+    end
+
+    context 'kwargs constraints' do
+      it "doesn't accept arbitrary kwargs" do
+        expect { subject.new(address: address, foobar: 5) }.to raise_error ArgumentError
+      end
+    end
+
+    describe "#lookup" do
+      subject(:weather_api) { weather_api_klass.new(address: build(:real_address)) }
+      let(:forecast) { subject.lookup }
+
+      context "success response" do
+        it "returns a hash" do
+          expect(subject.lookup).to be_a Hash
+        end
+
+        it "contains the weather forecast" do
+          expect(forecast).to include("forecast")
+        end
+
+        it "contains the weather location" do
+          expect(forecast).to include("location")
+        end
+
+        it "contains the weather alerts" do
+          expect(forecast).to include("alerts")
+        end
+      end
+    end
+  end
+end
